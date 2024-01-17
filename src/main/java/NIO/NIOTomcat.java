cd3b97c7f1e12aae5dd16c08437acc9fe33f9054
Ts <fwu@creams.io>
2024-01-17 14:29:43 +0800

    add NIO tomcat

--- src/main/java/NIO/NIOTomcat.java
+++ src/main/java/NIO/NIOTomcat.java
@@ -1,11 +1,8 @@
 package NIO;
 
 import java.io.IOException;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class NIOTomcat {
-    public static ConcurrentHashMap<String, Object> servletMap = new ConcurrentHashMap<>();
-    public static ConcurrentHashMap<String, String> uriMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
         NIOServer nioServer = new NIOServer(8080);
